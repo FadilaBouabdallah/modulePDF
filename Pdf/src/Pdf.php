3799b139675e8bff311071d578250441476e1289
FadilaBouabdallah <43574438+FadilaBouabdallah@users.noreply.github.com>
2018-11-20 13:10:29 +0100

    Add files via upload

---
+++ Pdf/src/Pdf.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pdf;
+
+use Omeka\Api\Representation\MediaRepresentation;
+use Omeka\Media\FileRenderer\RendererInterface;
+use Zend\View\Renderer\PhpRenderer;
+
+class PdfRenderer implements RendererInterface
+{
+    public function render(PhpRenderer $view, MediaRepresentation $media, array $options = [])
+    {
+        return sprintf(
+  
+            $view->escapeHtml($media->originalUrl())
+        );
+    }
+}
